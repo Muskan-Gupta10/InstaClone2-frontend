5c9ed0c23846a170f1780a936906bd8198065ada
Muskan-Gupta10 <125127176+Muskan-Gupta10@users.noreply.github.com>
2023-03-30 23:13:02 +0530

    files uploaded

---
+++ src/components/Landing_page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './LandingPage.css';
+import { NavLink } from 'react-router-dom';
+import mainImage from "./images/lens-1418954.png"
+
+ function Landing_page() {
+
+  return(
+    <div className='Container'>
+      <img id="image" src={mainImage} alt="image"/>
+      <p id='ptag'>10x Team 04
+      <br/>
+      <NavLink to="/Posts"><button id='btn'>Enter</button></NavLink>
+      </p>
+    </div>
+  );
+}
+export default Landing_page
---
+++ src/components/landing.js
@@ -0,0 +1,25 @@
+import "./landing.css"
+import { NavLink } from "react-router-dom"
+import mainImage from "./images/lens-1418954.png"
+
+
+
+const Landing = () => {
+    return (
+        <div className="landingpage">
+
+            <div className="welcomeimage">
+                <img height="100%" width="100%" src={mainImage} alt="Image" />
+            </div>
+
+
+            <div className="welcome">
+                <h1>welcome to 10x instaclone</h1>
+                <NavLink to="/allpost"> <button>Enter</button></NavLink>
+
+            </div>
+
+        </div>
+    )
+}
+export default Landing
